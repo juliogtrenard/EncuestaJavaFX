042dfe00f2d161b8e1f65c5cf96d2dfa1e23259e
Julio <juliogtrenard@gmail.com>
2024-10-07 11:53:38 +0200

    ADD tooltips para la lista y los sliders

--- src/main/java/es/juliogtrenard/encuestajavafx/HelloController.java
+++ src/main/java/es/juliogtrenard/encuestajavafx/HelloController.java
@@ -1,9 +1,7 @@
 package es.juliogtrenard.encuestajavafx;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
+import javafx.scene.control.*;
 
 public class HelloController {
     @FXML
@@ -13,11 +11,39 @@ public class HelloController {
     private ListView<Label> lstDeporte;
 
     @FXML
+    private Slider sliderCompras;
+
+    @FXML
+    private Slider sliderTv;
+
+    @FXML
+    private Slider sliderCine;
+
+    @FXML
     private void initialize() {
+        habilitarLista();
+        tooltips();
+    }
+
+    @FXML
+    private void habilitarLista() {
         lstDeporte.setDisable(true);
 
         chkDeporte.selectedProperty().addListener((observable, oldValue, newValue) -> {
             lstDeporte.setDisable(!newValue);
         });
+    }
+
+    @FXML
+    private void tooltips() {
+        Tooltip tooltipDeporte = new Tooltip("Marca el deporte que más te gusta.");
+        Tooltip tooltipCompras = new Tooltip("Indica del 1 al 10 cuánto te gusta ir de compras.");
+        Tooltip tooltipTv = new Tooltip("Indica del 1 al 10 cuánto te gusta ver televisión.");
+        Tooltip tooltipCine = new Tooltip("Indica del 1 al 10 cuánto te gusta ir al cine.");
+
+        lstDeporte.setTooltip(tooltipDeporte);
+        sliderCompras.setTooltip(tooltipCompras);
+        sliderTv.setTooltip(tooltipTv);
+        sliderCine.setTooltip(tooltipCine);
     }
 }
