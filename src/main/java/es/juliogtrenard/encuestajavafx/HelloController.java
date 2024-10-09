ca4f6fb604bc4e98c2d299fcddb13694d74634bb
Julio González <juliogtrenard@gmail.com>
2024-10-09 23:16:34 +0200

    ADD implementada ventana con datos de la encuesta y mensajes de error si no se completa algún campo

--- src/main/java/es/juliogtrenard/encuestajavafx/HelloController.java
+++ src/main/java/es/juliogtrenard/encuestajavafx/HelloController.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
 
+import java.text.DecimalFormat;
+
 public class HelloController {
     @FXML
     private CheckBox chkDeporte;
@@ -28,6 +30,12 @@ public class HelloController {
     private TextField txtNumHermanos;
 
     @FXML
+    private ComboBox<String> cbEdad;
+
+    @FXML
+    private ToggleGroup rbGroup;
+
+    @FXML
     private void initialize() {
         habilitarLista();
         tooltips();
@@ -35,7 +43,65 @@ public class HelloController {
 
     @FXML
     public void mostrarDatos(ActionEvent actionEvent) {
+        DecimalFormat df = new DecimalFormat("#.#");
 
+        String mensajeErrores = "";
+
+        String profesion = txtProfesion.getText();
+
+        int numHermanos = 0;
+
+        String edad = cbEdad.getSelectionModel().getSelectedItem();
+
+        RadioButton rbSeleccionado = (RadioButton) rbGroup.getSelectedToggle();
+
+        String rbValue = rbSeleccionado.getText();
+
+        String deporte = "";
+
+        double compras = sliderCompras.getValue();
+
+        double tv = sliderTv.getValue();
+
+        double cine = sliderCine.getValue();
+
+        if(profesion.isEmpty()) {
+            mensajeErrores += "Debes introducir tu profesión.\n";
+        }
+
+        try {
+            numHermanos = Integer.parseInt(txtNumHermanos.getText());
+        } catch (NumberFormatException e) {
+           mensajeErrores += "El campo 'Nº de hermanos' debe ser numérico.\n";
+        }
+
+        if(chkDeporte.isSelected()) {
+            try {
+                deporte = lstDeporte.getSelectionModel().getSelectedItem().getText();
+            } catch(NullPointerException e) {
+                mensajeErrores += "Debes seleccionar un deporte.\n";
+            }
+        }
+
+        if(mensajeErrores.isEmpty()) {
+            String mensaje = "Profesión: " + profesion + "\n" +
+                    "Nº de hermanos: " + numHermanos + "\n" +
+                    "Edad: " + edad + "\n" +
+                    "Sexo: " + rbValue + "\n" +
+                    "Deporte que practica: " + deporte + "\n" +
+                    "Grado de afición a las compras: " + df.format(compras) + "\n" +
+                    "Grado de afición a ver la televisión: " + df.format(tv) + "\n" +
+                    "Grado de afición a ir al cine: " + df.format(cine);
+            Alert alerta = new Alert(Alert.AlertType.INFORMATION, mensaje);
+            alerta.setHeaderText(null);
+            alerta.setTitle("Información de la encuesta:");
+            alerta.showAndWait();
+        } else {
+            Alert alerta = new Alert(Alert.AlertType.ERROR, mensajeErrores);
+            alerta.setHeaderText(null);
+            alerta.setTitle("Errores en la encuesta:");
+            alerta.showAndWait();
+        }
     }
 
     @FXML
