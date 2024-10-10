6028ccb0943a0fe809fad24deb64d447c0739135
Julio <juliogtrenard@gmail.com>
2024-10-10 13:31:17 +0200

    ADD documentación Javadoc en HelloApplication

--- src/main/java/es/juliogtrenard/encuestajavafx/HelloApplication.java
+++ src/main/java/es/juliogtrenard/encuestajavafx/HelloApplication.java
@@ -7,7 +7,17 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * Clase principal de la aplicación JavaFX para realizar una encuesta.
+ * Esta clase extiende la clase Application de JavaFX y configura el escenario principal.
+ */
 public class HelloApplication extends Application {
+    /**
+     * El método start para que la aplicación comience a ejecutarse.
+     *
+     * @param stage el escenario principal para esta aplicación, sobre el cual se puede establecer la escena de la aplicación.
+     * @throws IOException si no se puede cargar el archivo FXML
+     */
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
@@ -21,6 +31,12 @@ public class HelloApplication extends Application {
         stage.show();
     }
 
+    /**
+     * El punto de entrada principal para todas las aplicaciones JavaFX.
+     * Se llama desde el método main para iniciar la aplicación JavaFX.
+     *
+     * @param args los argumentos de línea de comandos pasados a la aplicación.
+     */
     public static void main(String[] args) {
         launch();
     }
